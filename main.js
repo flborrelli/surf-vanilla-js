99d3c08d35d2ac14a223e3b9fe76ca75b7da5f06
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-07-28 11:59:42 -0300

    On click event listener added to DD and DT

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-const tabImg = document.querySelectorAll('.js-tabmenu li img');
+const initTabNav = () =>{
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  const tabImg = document.querySelectorAll('.js-tabmenu li img');
+  
+  //Verificando se os elementos selecionados existem
+  if(tabMenu.length && tabContent.length && tabImg.length){
+    //Manter sempre o primeiro item de animais ativo
+    tabContent[0].classList.add('ativo');
+    tabImg[0].classList.add('no-opacity');
+    
+    //Ativar tabs conforme click
+    const activeTab = (index) => {
+      tabContent.forEach(element => element.classList.remove('ativo'))
+      tabContent[index].classList.add('ativo');
+    }
+    
+    //Ativar e remover  classe 'no-opacity' das imagens
+    const activeImg = (index) => {
+      tabImg.forEach(element => element.classList.remove('no-opacity'));
+      tabImg[index].classList.add('no-opacity');
+    }
+    
+    
+    //Ao clicar, executar as duas funções acima
+    tabMenu.forEach((item, i) => {
+      item.addEventListener('click', () => {
+        activeTab(i);
+        activeImg(i)
+      })
+    })
+  }
+}
+initTabNav();
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
 
 
-//Manter sempre o primeiro item de animais ativo
-tabContent[0].classList.add('ativo');
-tabImg[0].classList.add('no-opacity');
-
-//Ativar tabs conforme click
-const activeTab = (index) => {
-  tabContent.forEach(element => element.classList.remove('ativo'))
-  tabContent[index].classList.add('ativo');
+function activeAccordion(){
+  this.classList.toggle('ativo')
+  this.nextElementSibling.classList.toggle('ativo');
 }
 
-//Ativar e remover  classe 'no-opacity' das imagens
-const activeImg = (index) => {
-  tabImg.forEach(element => element.classList.remove('no-opacity'));
-  tabImg[index].classList.add('no-opacity');
-}
-
-
-//Ao clicar, executar as duas funções acima
-tabMenu.forEach((item, i) => {
-  item.addEventListener('click', () => {
-    activeTab(i);
-    activeImg(i)
-  })
+accordionList.forEach(item => {
+  item.addEventListener('click', activeAccordion);
 })
