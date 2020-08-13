43cafa9d8df076c5b97a56033a2aaad9bc2d462b
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-08-12 21:34:50 -0300

    Change JS classes for datase attributes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function initTabNav(){
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
-  const tabImg = document.querySelectorAll('.js-tabmenu li img');
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+  const tabImg = document.querySelectorAll('[data-tab="menu"] li img');
   
   //Verificando se os elementos selecionados existem
   if(tabMenu.length && tabContent.length && tabImg.length){
@@ -9,20 +9,24 @@ function initTabNav(){
     tabContent[0].classList.add('ativo');
     tabImg[0].classList.add('no-opacity');
     
-    //Ativar tabs conforme click
-    const activeTab = (index) => {
+    //Ativar tabs conforme click e adicionar classe show-right ou show-down
+    function activeTab(index){
       tabContent.forEach(element => element.classList.remove('ativo'))
-      tabContent[index].classList.add('ativo');
+      
+      //Selecionando cada dataset e cada elemento
+      const direcao = tabContent[index].dataset.anime;
+      //Adicionando a classe ativo e valor da chave data-set-anime ao elemento
+      tabContent[index].classList.add('ativo', direcao);
     }
     
     //Ativar e remover  classe 'no-opacity' das imagens
-    const activeImg = (index) => {
+    function activeImg(index) {
       tabImg.forEach(element => element.classList.remove('no-opacity'));
       tabImg[index].classList.add('no-opacity');
     }
     
     
-    //Ao clicar, executar as duas funções acima
+    //Ao clicar em cada item do array, pegar seu index e executar as duas funções acima passando esse index como argumento
     tabMenu.forEach((item, i) => {
       item.addEventListener('click', () => {
         activeTab(i);
@@ -34,7 +38,7 @@ function initTabNav(){
 initTabNav();
 
 function initAccordion(){
-  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
   const activeClass = 'ativo';
 
   if(accordionList.length){
@@ -54,7 +58,7 @@ function initAccordion(){
 initAccordion();
 
 function initSmoothScroll(){
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  const linksInternos = document.querySelectorAll('[data-menu="smooth"] a[href^="#"]');
   
   function scrollToSection(event) {
     event.preventDefault();
@@ -82,7 +86,7 @@ function initSmoothScroll(){
 }
 initSmoothScroll();
 
-const sections = document.querySelectorAll('.js-scroll');
+const sections = document.querySelectorAll('[data-anime="scroll"]');
 
 function initScrollAnimation(){
   if(sections.length){
@@ -105,3 +109,20 @@ function initScrollAnimation(){
   }
 }
 initScrollAnimation();
+
+
+
+
+// Adicione um atributo data-anime="show-down" e
+// data-anime="show-right" a todos as section's
+// com descricão dos animais.
+
+// Utilizando estes atributos, adicione a classe
+// show-down ou show-right a sua respectiva section
+// assim que a mesma aparecer na tela (animacao tab)
+
+// No CSS faça com que show-down anime de cima para baixo
+// e show-right continue com a mesma animação da esquerda
+// para a direita
+
+// Substitua todas as classes js- por data atributes.
